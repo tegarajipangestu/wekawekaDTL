18efbe649261359ceb61f0dfe43a2e6db092ecb0
tegarajipangestu <13512061@std.stei.itb.ac.id>
2015-09-28 14:33:41 +0700

    Making some dumb moves

---
+++ wekawekaDTL/src/main/java/weka/classifiers/trees/jkt48/JKT48TreeController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weka.classifiers.trees.jkt48;
+
+/**
+ *
+ * @author tegar
+ */
+public class JKT48TreeController {
+    
+    private JKT48ClassifierTree tree;
+    
+}
